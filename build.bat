19a1a600d4ae6c9455e198948d7966b04a8766aa
gab4000 <gabriel.cosme@orange.fr>
2025-05-25 13:34:30 +0200

    update windows compiler

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set RAYLIB_INCLUDE=C:\raylib\include
-set RAYLIB_LIB=C:\raylib\lib
+set RAYLIB_INCLUDE=C:\raylib\w64devkit\x86_64-w64-mingw32\include
+set RAYLIB_LIB=C:\raylib\w64devkit\x86_64-w64-mingw32\lib
 
 set CC=gcc
 set CFLAGS=-I"%RAYLIB_INCLUDE%" -L"%RAYLIB_LIB%" -lraylib -lopengl32 -lgdi32 -lwinmm -static
